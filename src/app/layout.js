cd06ec0db960067cd9ec388d2cd17f4303e450d9
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-04-29 18:12:49 +0600

    designed header.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-import Header from "../components/shared/Header";
-import NavBar from "../components/shared/NavBar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata = {
@@ -20,12 +14,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      data-theme="light"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" data-theme="light" className={`  h-full antialiased`}>
+      <body className={`${poppins.className} min-h-full flex flex-col`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { format, compareAsc } from "date-fns";
 
 const Header = () => {
-  return <div>Header.</div>;
+  return (
+    <div className=" flex flex-col justify-center items-center mt-6 space-y-2">
+      <Image src={logo} alt="logo" width={300} height={200} />
+      <p className="text-gray-500">Journalism Without Fear or Favour</p>
+      {/* <p>{format(new Date(), "<strong> EEEE</strong>,MMM dd,yyyy")}</p> */}
+      <p>
+        <strong>{format(new Date(), "EEEE")}</strong>,{" "}
+        {format(new Date(), "MMM dd,yyyy")}
+      </p>
+    </div>
+  );
 };
 
 export default Header;
