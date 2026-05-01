5eb70e328d35571efd2fc8239458641bf59fe488
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-05-01 23:17:44 +0600

    fixed navbar logout button

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -79,7 +79,7 @@ const NavBar = () => {
             />
             <button
               onClick={handleSignOut}
-              className="bg-red-600 text-white px-6 py-2 rounded-md hover:bg-red-700 transition"
+              className="bg-red-600 text-white px-6 py-2 rounded-md hover:bg-red-700 transition cursor-pointer"
             >
               Logout
             </button>
